2b9f131e6bc4f4c505641ec3de7f810c943fb2af
Diego Tavares <dtavares@imageworks.com>
2025-02-20 15:06:50 -0800

    Reorganize modules again

--- crates/rqd/src/system/running_frame.rs
+++ crates/rqd/src/system/running_frame.rs
@@ -13,36 +13,16 @@ use crate::config::config::RunnerConfig;
 /// Wrapper around protobuf message RunningFrameInfo
 #[derive(Clone)]
 pub struct RunningFrame {
-    pub resource_id: String,
-    pub job_id: Uuid,
-    pub job_name: String,
-    pub frame_id: Uuid,
-    pub frame_name: String,
-    pub layer_id: Uuid,
-    pub show: String,
-    pub shot: String,
-    pub command: String,
-    pub user_name: String,
-    pub num_cores: u32,
-    pub num_gpus: u32,
-    pub frame_temp_dir: String,
-    pub log_path: String,
-    pub requested_uid: Option<u32>,
-    pub gid: u32,
-    pub ignore_nimby: bool,
-    pub environment: HashMap<String, String>,
-    /// additional data can be provided about the running frame
-    pub attributes: HashMap<String, String>,
-    pub frame_stats: Option<FrameStats>,
-    initialized_resources: Option<InitializedResources>,
-}
-
-#[derive(Clone)]
-struct InitializedResources {
-    pub uid: u32,
-    pub config: RunnerConfig,
-    pub cpu_list: Vec<u32>,
-    pub gpu_list: Vec<u32>,
+    request: RunFrame,
+    job_id: Uuid,
+    frame_id: Uuid,
+    layer_id: Uuid,
+    frame_stats: Option<FrameStats>,
+    log_path: String,
+    uid: u32,
+    config: RunnerConfig,
+    cpu_list: Vec<u32>,
+    gpu_list: Vec<u32>,
 }
 
 #[derive(Clone)]
@@ -87,6 +67,34 @@ impl Default for FrameStats {
 }
 
 impl RunningFrame {
+    pub fn init(
+        request: RunFrame,
+        uid: u32,
+        config: RunnerConfig,
+        cpu_list: Vec<u32>,
+        gpu_list: Vec<u32>,
+    ) -> Self {
+        let job_id = request.job_id();
+        let frame_id = request.frame_id();
+        let layer_id = request.layer_id();
+        let log_path = std::path::Path::new(&request.log_dir)
+            .join(format!("{}.{}.rqlog", request.job_name, request.frame_name))
+            .to_string_lossy()
+            .to_string();
+        RunningFrame {
+            request,
+            job_id,
+            frame_id,
+            layer_id,
+            frame_stats: None,
+            log_path,
+            uid,
+            config,
+            cpu_list,
+            gpu_list,
+        }
+    }
+
     #[cfg(any(target_os = "linux", target_os = "macos"))]
     pub fn run(&self) -> Result<()> {
         // Windows, Linux or Docker
@@ -94,7 +102,7 @@ impl RunningFrame {
         //  - log path
         //  - Create log stream
 
-        if self.resources().config.run_on_docker {
+        if self.config.run_on_docker {
             return self.run_on_docker();
         }
 
@@ -113,94 +121,30 @@ impl RunningFrame {
     pub fn kill(&self) {
         todo!()
     }
-
-    fn resources(&self) -> &InitializedResources {
-        self.initialized_resources.as_ref().expect(
-            "Running Frame should have initialized resources at this point. Unexpected behaviour!",
-        )
-    }
-
-    pub fn with_resources(
-        self,
-        config: RunnerConfig,
-        uid: u32,
-        cpu_list: Vec<u32>,
-        gpu_list: Vec<u32>,
-    ) -> Self {
-        RunningFrame {
-            initialized_resources: Some(InitializedResources {
-                uid,
-                config,
-                cpu_list,
-                gpu_list,
-            }),
-            ..self
-        }
-    }
 }
 
 impl From<RunningFrame> for RunningFrameInfo {
-    fn from(frame_info: RunningFrame) -> Self {
-        let frame_stats = frame_info.frame_stats.unwrap_or(FrameStats::default());
+    fn from(running_frame: RunningFrame) -> Self {
+        let frame_stats = running_frame.frame_stats.unwrap_or(FrameStats::default());
         RunningFrameInfo {
-            resource_id: frame_info.resource_id.clone(),
-            job_id: frame_info.job_id.to_string(),
-            job_name: frame_info.job_name.clone(),
-            frame_id: frame_info.frame_id.to_string(),
-            frame_name: frame_info.frame_name.clone(),
-            layer_id: frame_info.layer_id.to_string(),
-            num_cores: frame_info.num_cores as i32,
+            resource_id: running_frame.request.resource_id.clone(),
+            job_id: running_frame.request.job_id.to_string(),
+            job_name: running_frame.request.job_name.clone(),
+            frame_id: running_frame.request.frame_id.to_string(),
+            frame_name: running_frame.request.frame_name.clone(),
+            layer_id: running_frame.request.layer_id.to_string(),
+            num_cores: running_frame.request.num_cores as i32,
             start_time: frame_stats.epoch_start_time as i64,
             max_rss: frame_stats.max_rss as i64,
             rss: frame_stats.rss as i64,
             max_vsize: frame_stats.max_vsize as i64,
             vsize: frame_stats.vsize as i64,
-            attributes: frame_info.attributes.clone(),
+            attributes: running_frame.request.attributes.clone(),
             llu_time: frame_stats.llu_time as i64,
-            num_gpus: frame_info.num_gpus as i32,
+            num_gpus: running_frame.request.num_gpus as i32,
             max_used_gpu_memory: frame_stats.max_used_gpu_memory as i64,
             used_gpu_memory: frame_stats.used_gpu_memory as i64,
             children: frame_stats.children.clone(),
-        }
-    }
-}
-
-impl From<RunFrame> for RunningFrame {
-    fn from(run_frame: RunFrame) -> Self {
-        let job_id = run_frame.job_id();
-        let frame_id = run_frame.frame_id();
-        let layer_id = run_frame.layer_id();
-        let log_path = std::path::Path::new(&run_frame.log_dir)
-            .join(format!(
-                "{}.{}.rqlog",
-                run_frame.job_name, run_frame.frame_name
-            ))
-            .to_string_lossy()
-            .to_string();
-        RunningFrame {
-            resource_id: run_frame.resource_id,
-            job_id,
-            job_name: run_frame.job_name.clone(),
-            frame_id,
-            frame_name: run_frame.frame_name.clone(),
-            layer_id,
-            command: run_frame.command,
-            user_name: run_frame.user_name,
-            show: run_frame.show,
-            shot: run_frame.shot,
-            frame_temp_dir: run_frame.frame_temp_dir,
-            log_path,
-            num_cores: run_frame.num_cores as u32,
-            requested_uid: run_frame.uid_optional.map(|optional| match optional {
-                opencue_proto::rqd::run_frame::UidOptional::Uid(uid) => uid as u32,
-            }),
-            gid: run_frame.gid as u32,
-            ignore_nimby: run_frame.ignore_nimby,
-            environment: run_frame.environment,
-            attributes: run_frame.attributes,
-            num_gpus: run_frame.num_gpus as u32,
-            frame_stats: None,
-            initialized_resources: None,
         }
     }
 }
@@ -236,7 +180,7 @@ impl RunningFrameCache {
         self.cache.insert(running_frame.frame_id, running_frame)
     }
 
-    pub fn contains(&self, running_frame: &RunningFrame) -> bool {
-        self.cache.contains_key(&running_frame.frame_id)
+    pub fn contains(&self, frame_id: &Uuid) -> bool {
+        self.cache.contains_key(frame_id)
     }
 }
